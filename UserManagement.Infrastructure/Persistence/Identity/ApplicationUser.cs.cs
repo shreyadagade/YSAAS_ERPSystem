28879fbc92f2d95d72e643bb1cb00e7fabe002d5
Shreya Dagde <shreyadagade07@gmail.com>
2026-08-08 18:39:41 +0530

    Describe your changes

---
+++ UserManagement.Infrastructure/Persistence/Identity/ApplicationUser.cs.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManagement.Infrastructure.Persistence.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
