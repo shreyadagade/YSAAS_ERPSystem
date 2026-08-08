9fdc5c2e69872f2473a0b97fc44b0b495797083e
shreyadagade <shreyadagade07@gmail.com>
2026-08-08 18:39:41 +0530

    Describe your changes

---
+++ UserManagement.Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManagement.Infrastructure.Persistence.Context
+{
+    internal class ApplicationDbContext
+    {
+    }
+}
